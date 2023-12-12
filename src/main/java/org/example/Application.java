17f46d420039c30420e853375ee7c3450b0dafdf
Riccardo Di Bari <riccardodibari@hotmail.it>
2023-12-12 23:34:04 +0100

    Update Application.java

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestioneeventi");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       
     }
 }
